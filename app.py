2ef92e8990b6a84f4362d3cd6f83540693b36253
BitZeus <134755741+BitZeus@users.noreply.github.com>
2023-07-12 19:25:08 +0100

    Adicionado ficheiro desafio4.jpg e pagina desafio4.html bem como função desafio4() no ficheiro app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from forms import RespostaForm, AvancarForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'segredo'
-respostas = ['co2', 'solar', 'electricos']  # Lista que contem as respostas
+respostas = ['co2', 'solar', 'electricos', 'reciclar']  # Lista que contem as respostas
 
 # Função que redireciona para a página home.html (inicio)
 @app.route("/", methods=['GET', 'POST'])
@@ -40,13 +40,22 @@ def desafio2():
 def desafio3():
     form = RespostaForm()
     if form.validate_on_submit():  # Ao pressionar o botao executa as operações abaixo
-        if form.resposta.data.lower() == respostas[2]:  # Verifica se a resposta corresponde na lista de respostas na posição 1
-            return redirect(url_for('fim'))  # Redireciona para a pagina fim se resposta correcta
+        if form.resposta.data.lower() == respostas[2]:  # Verifica se a resposta corresponde na lista de respostas na posição 2
+            return redirect(url_for('desafio4'))  # Redireciona para a pagina desafio4 se resposta correcta
         else:
             flash('Resposta incorreta. Tenta novamente.', 'error')
     return render_template('desafio3.html', form=form)  # Usa a template definida para a pagina desafio3
-@app.route("/fim", methods=['GET', 'POST'])  # Decorador para a pagina fim
+@app.route("/desafio4", methods=['GET', 'POST'])  # Decorador para a pagina desafio4
 
+def desafio4():
+    form = RespostaForm()
+    if form.validate_on_submit():  # Ao pressionar o botao executa as operações abaixo
+        if form.resposta.data.lower() == respostas[3]:  # Verifica se a resposta corresponde na lista de respostas na posição 3
+            return redirect(url_for('fim'))  # Redireciona para a pagina fim se resposta correcta
+        else:
+            flash('Resposta incorreta. Tenta novamente.', 'error')
+    return render_template('desafio4.html', form=form)  # Usa a template definida para a pagina desafio4
+@app.route("/fim", methods=['GET', 'POST'])  # Decorador para a pagina fim
 
 # Função que redireciona para a página fim.html
 def fim():
